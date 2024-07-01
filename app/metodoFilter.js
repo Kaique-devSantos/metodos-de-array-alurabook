fcfa98fdaea74ec0657decc6af101ca31ea7c9f8
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-06-30 22:15:44 -0300

    novas funcionalidades

---
+++ app/metodoFilter.js
@@ -0,0 +1,39 @@
+
+
+// const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
+
+// btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront);
+
+// function filtrarLivrosDeFront(){
+//     let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end');
+//     console.table(livrosFiltrados);
+// }
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : FiltrarPorCategoria(categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function FiltrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+}
